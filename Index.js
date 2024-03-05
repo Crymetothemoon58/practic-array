200339e9529e87aee67400eea084ad3c2e1f9562
David Alejandro Arteaga <davidalejandroarteaga3@gmail.com>
2024-03-05 18:12:16 -0500

    guardado de arry con multiples elementos

--- Index.js
+++ Index.js
@@ -7,3 +7,5 @@ console.log(arrayVacio);
 var numeros = [5, 10, 15, 20];
 console.log(numeros);
 
+var elementosVariados = [1, "Hola", true, 2, { nombre: "Objeto" }];
+console.log(elementosVariados);
