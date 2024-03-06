b378ad0c6d48d64bbac89d16888ac52aa9d24293
David Alejandro Arteaga <davidalejandroarteaga3@gmail.com>
2024-03-05 21:03:34 -0500

    adicion de arrays slice

--- Index.js
+++ Index.js
@@ -88,3 +88,11 @@ function generarArray(longitud) {
   console.log(primerElemento); 
   console.log(myArray); 
   
+
+  let myArray = [1, 2, 3, 4, 5];
+
+
+let subArray = myArray.slice(1, 4);
+
+console.log(subArray); 
+console.log(myArray); 
